cc7ac1895b5d421a1dcaac3054c89a93dd4b8305
Eyman21 <107034535+Eyman21@users.noreply.github.com>
2022-06-20 09:34:52 +0200

    Update my_putnbr.c

--- Day01/my_putnbr/my_putnbr.c
+++ Day01/my_putnbr/my_putnbr.c
@@ -1,21 +1,48 @@
 #include <unistd.h>
+#include <stdio.h>
 
 void my_putchar(char c)
 {
-	write(1, &c, 1);
-}	
-
-int my_putnbr(int n) // n = 3
-{
-	// ici faut faire de la magie
-
-	n =  '0' % n;
-   	my_putchar(n); // tu veux n = 51
-	my_putchar('\n');
+    write(1, &c, 1);
 }
 
-int main(int argc, char const **argv)
+int get_dec(int nb)
 {
-	my_putnbr(868156746);
-	return 0;
+    int count = 0; 
+    int  diviseur = 1;
+    while (nb >= 10)
+    { 
+        nb = nb / 10;
+        count = count + 1; 
+        diviseur = diviseur * 10;
+    }
+    return (diviseur);
+} 
+
+int my_putnbr(int nb)
+{
+    int diviseur; 
+    int digit;
+    char c; 
+
+    if (nb < 0)
+    {
+        my_putchar('-');
+        nb = nb * -1;
+    }
+	
+    diviseur = get_dec(nb);
+    while(diviseur >= 1)
+    {
+        digit = nb / diviseur;
+        c = digit + 48;
+        my_putchar(c);
+        nb = nb % diviseur;
+        diviseur = diviseur / 10;
+    }
+}
+
+int main(void)
+{  
+    my_putnbr(-14205);
 }
